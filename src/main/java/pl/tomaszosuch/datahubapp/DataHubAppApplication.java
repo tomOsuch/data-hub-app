db0ed6bffe877e669281da4966bbf2653a3526af
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-09-02 22:51:54 +0200

    first commit

---
+++ src/main/java/pl/tomaszosuch/datahubapp/DataHubAppApplication.java
@@ -0,0 +1,13 @@
+package pl.tomaszosuch.datahubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataHubAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataHubAppApplication.class, args);
+    }
+
+}
